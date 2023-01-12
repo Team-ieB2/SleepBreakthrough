10c90d94a120de5487e5f142fd9c8ea637093562
Tomoya Nakada <e215725@ie.u-ryukyu.ac.jp>
2023-01-12 19:53:52 +0900

    add: gpiotest.py

---
+++ test/GPIO/gpiotest.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+ 
+import RPi.GPIO as GPIO
+import time
+ 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.OUT)
+ 
+for i in range(5):
+    GPIO.output(17, True)
+    time.sleep(3)
+    GPIO.output(17, False)
+    time.sleep(3)
+    
+GPIO.cleanup()
