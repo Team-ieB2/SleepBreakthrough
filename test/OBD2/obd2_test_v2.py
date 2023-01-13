c4df3c8a722f8ba55fef9c8aac0ab27691bf4f72
CrabAnderson <crab85193@gmail.com>
2023-01-14 03:09:44 +0900

    fix: delete unnecessary processes.

--- test/OBD2/obd2_test_v2.py
+++ test/OBD2/obd2_test_v2.py
@@ -1,6 +1,9 @@
 import obd
 from obd import OBDStatus
 import csv
+import datetime
+import pytz
+import sys
 
 class OBD2():
     def __init__(self):
@@ -17,27 +20,32 @@ class OBD2():
 
     def write_csv(self, name, value):
         if self.writer:
-            self.writer.writerow([name, value])
+            now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+            self.writer.writerow([now, name, value])
 
     def get_rpm(self):
         if self.connection.status() == OBDStatus.CAR_CONNECTED:
             rpm = self.connection.query(obd.commands.RPM)
-            self.write_csv("RPM",rpm)
-            return rpm
+            self.write_csv("RPM",rpm.value.magnitude)
+            return rpm.value.magnitude
         else:
             pass
 
     def get_speed(self):
         if self.connection.status() == OBDStatus.CAR_CONNECTED:
             speed = self.connection.query(obd.commands.SPEED)
-            self.write_csv("SPEED",speed)
-            return speed
+            self.write_csv("SPEED",speed.value.magnitude)
+            return speed.value.magnitude
         else:
             pass
 
 if __name__ == "__main__":
     obd2 = OBD2()
-    # obd2.open_csv()
-    while True:
-        print(f"RPM: {obd2.get_rpm()}")
-        print(f"SPEED: {obd2.get_speed()}")
+    try:
+        # obd2.open_csv()
+        while True:
+            print(f"RPM: {obd2.get_rpm()}")
+            print(f"SPEED: {obd2.get_speed()}")
+    except:
+        obd2.exit()
+        sys.exit()
