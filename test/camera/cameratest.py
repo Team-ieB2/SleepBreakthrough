6e97753b474768f5c1370ae876324a4cc25c2c50
Tomoya Nakada <e215725@ie.u-ryukyu.ac.jp>
2023-01-13 17:19:50 +0900

    fix: fix cameratest.py

--- test/camera/cameratest.py
+++ test/camera/cameratest.py
@@ -3,14 +3,21 @@
 import cv2
 import time
 import numpy as np
+import os
+import pathlib
 
+# 顔認識プログラムのパス(絶対パス)
+face_cascade_path = os.path.abspath("learningdata/haarcascade_frontalface_alt.xml")
+face_cascade_path = str(pathlib.PurePath(face_cascade_path))
+
+# 目認識プログラムのパス(絶対パス)
+eye_cascade_path = os.path.abspath("learningdata/haarcascade_eye_tree_eyeglasses.xml")
+eye_cascade_path = str(pathlib.PurePath(eye_cascade_path))
 
 def camera_recognition():
+
   # デバイスを読み込む
   camera = cv2.VideoCapture(0)
-
-  # 初回だけ判定する変数
-  at_first = 0
 
   # 時間の配列の場所指定
   index = 0
@@ -18,11 +25,11 @@ def camera_recognition():
   # 目を閉じた回数を記録
   eye_close_count = 0
 
+  # ループ回数が一定に達したかどうか判定する
+  judge = 0
+
   # 頭を下げた回数を記録
   head_down_count = 0
-
-  # 目を閉じた時間を計測する配列
-  eye_close_times = []
 
   # 顔の枠面積を保存する配列
   face_frame_area = []
@@ -30,21 +37,33 @@ def camera_recognition():
   # 顔だけで判定させるかどうかを決めるfrag
   eye_frag = True
 
-  # 顔認識プログラムの呼び出し
-  face_cascade = cv2.CascadeClassifier('learningdata/haarcascade_frontalface_alt.xml')
+  # 顔認識プログラムの呼び出し(プログラム単体呼び出しの場合)
+  # face_cascade = cv2.CascadeClassifier('learningdata/haarcascade_frontalface_alt.xml')
 
-  # 目認識プログラムの呼び出し
-  eye_cascade = cv2.CascadeClassifier('learningdata/haarcascade_eye.xml')
+  # 顔認識プログラムの呼び出し(関数呼び出しの場合)
+  face_cascade = cv2.CascadeClassifier(face_cascade_path)
+
+  # 目認識プログラムの呼び出し(プログラム単体呼び出しの場合)
+  # eye_cascade = cv2.CascadeClassifier('learningdata/haarcascade_eye_tree_eyeglasses.xml')
+
+  # 目認識プログラムの呼び出し(関数呼び出しの場合)
+  eye_cascade = cv2.CascadeClassifier(eye_cascade_path)
 
   # 目認識プログラムの呼び出し(メガネの場合)
   # glass_cascade = cv2.CascadeClassifier('learningdata/haarcascade_eye_tree_eyeglasses.xml')
 
   # 時間計測開始
-  time_start = time.time()
+  time.time()
+  times = 0
 
   while True:
+    # 現在の時間を取得する
     now_time = time.time()
-    frag = True
+
+    times += now_time
+
+    # return するかどうかのfrag
+    frag = False
 
     # 画像の取り出し
     ret, frame = camera.read()
@@ -58,79 +77,96 @@ def camera_recognition():
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # 認識したものの枠の色を決める
-    recognition_frame = (0, 0, 255)
+    # recognition_frame = (0, 0, 255)
+
+    face_frame = (255, 0, 0)
+    eye_frame = (0, 0, 255)
 
     # 顔認識の実行
-    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minSize=(50,50))
 
     # 目認識の実行
-    eyes = eye_cascade.detectMultiScale(gray, 1.1, 5)
+    eyes = eye_cascade.detectMultiScale(gray, minSize=(75,75))
 
     # (初回時のみ)目認識ができなかった場合(メガネ・サングラスなどで認識不可だった)にeye_fragをおろす
-    if at_first == 0 and len(eyes) == 0:
-      eye_frag = False
+    if judge <= 25:
+      if len(eyes) == 0:
+        eye_frag = False
+      else:
+        eye_frag = True
 
     # 認識した部分(顔)に枠と色をつける
     for (x,y,w,h) in faces:
-      cv2.rectangle(frame, (x, y), (x+w, y+h), recognition_frame, 2)
-      print(x, y, w, h)
+      # cv2.rectangle(frame, (x, y), (x+w, y+h), recognition_frame, 2)
+      cv2.rectangle(frame, (x, y), (x+w, y+h), face_frame, 2)
+      # print(w*h)
     
     # 認識した部分(目)に枠と色をつける
     for (x,y,w,h) in eyes:
-      cv2.rectangle(frame, (x, y), (x+w, y+h), recognition_frame, 2)
+      # cv2.rectangle(frame, (x, y), (x+w, y+h), recognition_frame, 2)
+      cv2.rectangle(frame, (x, y), (x+w, y+h), eye_frame, 2)
 
     cv2.imshow("FaceRecognitionSystem", frame)
     key = cv2.waitKey(1)
 
     # 目を認識できた場合
-    if eye_frag:
+    if eye_frag and judge > 25:
       # 目を閉じている場合
       if len(eyes) < 2 and len(faces) >= 1:
-        eye_close_times.append(time.time()-now_time)
+        closed_time = time.time()
+        times -= closed_time
+        print("eye: " + str(eye_close_count))
 
-        if index == 0:
-          continue
+        if times < 0:
+          eye_close_count += 1
+          times = 0
         else:
-          if eye_close_times[index] - eye_close_times[index-1] < 0.5:
-            eye_close_count += 1
+          eye_close_count = 0
+        
+        if eye_close_count >= 100:
+          frag = True
+          break
+
+        times = 0
+    # 顔の下がった回数をカウント
+    elif judge > 25:
+      if len(faces) != 0:
+        for (x, y, w, h) in faces:
+          face_frame_area.append(w * h)
+          frame_avg = np.mean(face_frame_area)
+          index += 1
+      
+        if index >= 2 and len(face_frame_area) > 2:
+          print(head_down_count)
+          # face_diff = face_frame_area[index-1] - face_frame_area[index-2]
+          face_diff =  w * h - frame_avg
+          if face_diff < -10000:
+            head_down_count += 1
+            
           else:
-            eye_close_count = 0
-            eye_close_times = []
+            face_frame_area = []
             index = 0
             continue
-        
-        if eye_close_count >= 300:
-          frag = True
-          break
 
-        index += 1
-    # 顔の下がった回数をカウント
-    else:
-      face_frame_area.append(faces[-2]*faces[-1])
-      frame_avg = np.mean(face_frame_area)
+          if head_down_count >= 50:
+            frag = True
+            break
 
-      if index <= 2:
-        continue
-
-      else:
-        face_diff = face_frame_area[index] - face_frame_area[index-1]
-        if face_diff < frame_avg:
-          frag = True
-          break
-        else:
-          face_frame_area = []
-          index = 0
-          continue
-
-      index += 1
-    
-    at_first += 1
+    judge += 1
 
     # Escキーを入力されたら画面を閉じる
     if key == 27:
       break
 
-    return frag
-
   camera.release()
   cv2.destroyAllWindows()
+
+  return frag
+
+
+if __name__ == "__main__":
+  camera_recognition()
+
+if __name__ == "camera.cameratest":
+  face_cascade_path = os.path.abspath("camera/learningdata/haarcascade_frontalface_alt.xml")
+  eye_cascade_path = os.path.abspath("camera/learningdata/haarcascade_eye_tree_eyeglasses.xml")
