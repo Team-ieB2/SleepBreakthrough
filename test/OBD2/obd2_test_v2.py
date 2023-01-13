f83add2925892aecbf8f9cb093c835b7838e0801
CrabAnderson <crab85193@gmail.com>
2023-01-14 03:50:55 +0900

    update: add exception handling.

--- test/OBD2/obd2_test_v2.py
+++ test/OBD2/obd2_test_v2.py
@@ -7,36 +7,51 @@ import sys
 
 class OBD2():
     def __init__(self):
-        self.connection = obd.OBD("/dev/rfcomm0")
-        self.writer = None
-        print(self.connection.status())
+        try:
+            self.connection = obd.OBD("/dev/rfcomm0")
+            self.writer = None
+            print(self.connection.status())
+        except:
+            pass
 
     def exit(self):
         self.connection.close()
 
     def open_csv(self, file_name: str = "data.csv"):
-        f = open(file_name, "w")
-        self.writer = csv.writer(f, lineterminator="\n")
+        try:
+            f = open(file_name, "w")
+            self.writer = csv.writer(f, lineterminator="\n")
+        except:
+            pass
 
     def write_csv(self, name, value):
-        if self.writer:
-            now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
-            self.writer.writerow([now, name, value])
+        try:
+            if self.writer:
+                now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+                self.writer.writerow([now, name, value])
+        except:
+            pass
 
     def get_rpm(self):
-        if self.connection.status() == OBDStatus.CAR_CONNECTED:
-            rpm = self.connection.query(obd.commands.RPM)
-            self.write_csv("RPM",rpm.value.magnitude)
-            return rpm.value.magnitude
-        else:
+        try:
+            if self.connection.status() == OBDStatus.CAR_CONNECTED:
+                rpm = self.connection.query(obd.commands.RPM)
+                self.write_csv("RPM",rpm.value.magnitude)
+                return rpm.value.magnitude
+            else:
+                pass
+        except:
             pass
 
     def get_speed(self):
-        if self.connection.status() == OBDStatus.CAR_CONNECTED:
-            speed = self.connection.query(obd.commands.SPEED)
-            self.write_csv("SPEED",speed.value.magnitude)
-            return speed.value.magnitude
-        else:
+        try:
+            if self.connection.status() == OBDStatus.CAR_CONNECTED:
+                speed = self.connection.query(obd.commands.SPEED)
+                self.write_csv("SPEED",speed.value.magnitude)
+                return speed.value.magnitude
+            else:
+                pass
+        except:
             pass
 
 if __name__ == "__main__":
