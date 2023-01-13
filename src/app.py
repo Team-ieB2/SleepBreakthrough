5937a96891f3120a02fe89cc861eb3553b9366b9
CrabAnderson <crab85193@gmail.com>
2023-01-13 21:27:55 +0900

    add: add main python file.

---
+++ src/app.py
@@ -0,0 +1,37 @@
+"""
+app.py
+Main Application.
+
+Copyright (C) 2023 Team ieB2. All Rights Reserved.
+"""
+class SleepBreakthrough():
+    """
+    メインクラス
+    """
+    def __init__(self):
+        """
+        コンストラクタ
+        """
+        pass
+
+    def run():
+        """
+        アプリケーションを起動する
+        """
+        pass
+
+    def exit():
+        """
+        アプリケーションを終了する
+        """
+        pass
+
+    def _exit():
+        """
+        アプリケーションを強制終了する
+        """
+        pass
+
+if __name__ == "__main__":
+    App = SleepBreakthrough()
+    App.run()
