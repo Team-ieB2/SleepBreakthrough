5ff2d36493284d8601e0cf17922bcd33bd94946b
CrabAnderson <crab85193@gmail.com>
2023-01-14 03:41:49 +0900

    fix: typo

--- test/camera/camera_test_v2.py
+++ test/camera/camera_test_v2.py
@@ -8,7 +8,7 @@ class CameraTest():
     def __init__(self):
         print("カメラシステムの初期化開始")
         self.time = 0.0
-        self.camera = None
+        self.camera = cv2.VideoCapture(0)
         self.head_down_count = 0
         self.ret = None
         self.back = None
@@ -25,12 +25,11 @@ class CameraTest():
         self.face_area = []
         self.sleep_frag = False
         print("カメラシステム初期化完了")
-    
+
     def launch_camera(self):
-        self.camera = cv2.VideoCapture(0)
         for _ in range(30):
             self.ret, self.back = self.camera.read()
-        
+
         self.ret, self.frame = self.camera.read()
         if not self.ret:
             return True
@@ -38,18 +37,18 @@ class CameraTest():
         self.gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
         self.face_cascade = cv2.CascadeClassifier(self.face_cascade_path)
         self.eye_cascade = cv2.CascadeClassifier(self.eye_cascade_path)
-    
+
     def surround_face(self):
         for (x, y, w, h) in self.get_faces():
             cv2.rectangle(self.frame, (x, y), (x+w, y+h), self.face_frame, 2)
-    
+
     def surround_eyes(self):
         for (x, y, w, h) in self.get_eyes():
             cv2.rectangle(self.frame, (x, y), (x+w, y+h), self.eye_frame, 2)
 
     def count_close_eye_time(self):
         return time.perf_counter()
-    
+
 
     def count_head_down(self):
         self.head_down_count += 1
@@ -63,7 +62,7 @@ class CameraTest():
         if len(self.get_faces()) != 0:
             for (x, y, w, h) in self.get_faces():
                 self.add_face_area(y)
-                self.culc_face_avg(self.get_face_area())
+                self.calc_face_avg(self.get_face_area())
 
             self.set_face_diff(y - self.get_face_avg())
 
@@ -73,12 +72,12 @@ class CameraTest():
     def add_face_area(self, area):
         self.face_area.append(area)
 
-    def culc_face_avg(self, face_area):
+    def calc_face_avg(self, face_area):
         self.face_avg = np.mean(face_area)
 
     def set_faces(self):
         self.faces = self.face_cascade.detectMultiScale(self.gray, scaleFactor=1.1, minSize=(50,50))
-    
+
     def set_eyes(self):
         self.eyes = self.eye_cascade.detectMultiScale(self.gray, minSize=(50,50))
 
@@ -93,28 +92,28 @@ class CameraTest():
 
     def get_count_close_eye(self):
         return self.count_close_eye
-    
+
     def get_head_down_count(self):
         return self.head_down_count
 
     def get_face_area(self):
         return self.face_area
-    
+
     def get_face_avg(self):
         return self.face_avg
-    
+
     def get_face_diff(self):
         return self.face_diff
 
     def get_eyes(self):
         return self.eyes
-    
+
     def get_faces(self):
         return self.faces
 
     def get_time(self):
         return self.time
-    
+
     def get_sleep_frag(self):
         return self.sleep_frag
 
