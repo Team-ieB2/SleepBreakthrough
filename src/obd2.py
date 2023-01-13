4db5ba9954eada7fffe747196791a957378f9c7d
CrabAnderson <crab85193@gmail.com>
2023-01-14 03:51:30 +0900

    add: add src/obd2.py

---
+++ src/obd2.py
@@ -0,0 +1,72 @@
+"""
+obd2.py
+OBD2 Controller.
+
+Copyright (C) 2023 Team ieB2. All Rights Reserved.
+"""
+import obd
+from obd import OBDStatus
+import csv
+import datetime
+import pytz
+import sys
+
+class OBD2():
+    def __init__(self):
+        try:
+            self.connection = obd.OBD("/dev/rfcomm0")
+            self.writer = None
+            print(self.connection.status())
+        except:
+            pass
+
+    def exit(self):
+        self.connection.close()
+
+    def open_csv(self, file_name: str = "data.csv"):
+        try:
+            f = open("./csv/" + file_name, "w")
+            self.writer = csv.writer(f, lineterminator="\n")
+        except:
+            pass
+
+    def write_csv(self, name, value):
+        try:
+            if self.writer:
+                now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+                self.writer.writerow([now, name, value])
+        except:
+            pass
+
+    def get_rpm(self) -> int:
+        try:
+            if self.connection.status() == OBDStatus.CAR_CONNECTED:
+                rpm = self.connection.query(obd.commands.RPM)
+                self.write_csv("RPM",rpm.value.magnitude)
+                return rpm.value.magnitude
+            else:
+                pass
+        except:
+            pass
+
+    def get_speed(self) -> int:
+        try:
+            if self.connection.status() == OBDStatus.CAR_CONNECTED:
+                speed = self.connection.query(obd.commands.SPEED)
+                self.write_csv("SPEED",speed.value.magnitude)
+                return speed.value.magnitude
+            else:
+                pass
+        except:
+            pass
+
+if __name__ == "__main__":
+    obd2 = OBD2()
+    try:
+        obd2.open_csv()
+        while True:
+            print(f"RPM: {obd2.get_rpm()}")
+            print(f"SPEED: {obd2.get_speed()}")
+    except:
+        obd2.exit()
+        sys.exit()
